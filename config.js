7e64b6d2d113a5a1f90375ae88c2bd5e3086068d
Levi <leviboyd745@gmail.com>
2024-08-17 19:12:05 +1000

    timmy test added badword3

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // config.js
 module.exports = {
-    botToken: process.env.BOT_TOKEN,
-    badWords: ['badword1', 'badword2'],
-    allowedUsers: ['123456789012345678', '987654321098765432'],
-    startupChannelId: process.env.STARTUP_CHANNEL_ID
+  botToken: process.env.BOT_TOKEN,
+  badWords: ["badword1", "badword2", "badword3"],
+  allowedUsers: ["123456789012345678", "987654321098765432"],
+  startupChannelId: process.env.STARTUP_CHANNEL_ID,
 };
